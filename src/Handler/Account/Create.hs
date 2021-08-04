dd2f5b634247aa8606422fb6235fec513827b32c
Vladimir Ciobanu <admin@cvlad.info>
2021-08-04 19:20:01 +0300

    Insert organisation and relationships when creating a new user.

--- src/Handler/Account/Create.hs
+++ src/Handler/Account/Create.hs
@@ -1,6 +1,9 @@
 module Handler.Account.Create where
 
+import DB.Organisation
 import DB.User
+import Data.Time
+import Data.UUID.V4 (nextRandom)
 import ImportYesod
 import Model.UserModel
 import Web.Form
@@ -15,6 +18,28 @@ postAccountCreateR = do
       putAssign "form_error" "true"
       handleFormErrors errors
       redirect SignupR
-    Result user -> do
-      runDB $ insertUser user
+    Result user@User{..} -> do
+      orgId <- liftIO $ OrganisationId <$> nextRandom
+      userOrgId <- liftIO $ UserOrganisationId <$> nextRandom
+      timestamp <- liftIO getCurrentTime
+      let org = newOrganisationFor orgId timestamp user
+      runDB
+        $ insertUser user
+        *> insertOrganisation org
+        *> attachUser userId orgId userOrgId
       redirect HomeR
+  where
+    newOrganisationFor :: OrganisationId -> UTCTime -> User -> Organisation
+    newOrganisationFor organisationId createdAt user =
+      Organisation
+        { organisationId
+        , organisationName = newOrgNameFrom user
+        , createdAt
+        , updatedAt = createdAt
+        }
+
+    newOrgNameFrom :: User -> Text
+    newOrgNameFrom User{..} = userOrgNamePrefix <> username
+
+    userOrgNamePrefix :: Text
+    userOrgNamePrefix = "default_org_for"
