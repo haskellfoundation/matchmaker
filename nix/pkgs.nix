ad4af685b48359386131371d6b7d061b33210d91
Evan Relf <evan@evanrelf.com>
2021-08-05 13:17:34 -0700

    Move Nixpkgs pin to separate file

---
+++ nix/pkgs.nix
@@ -0,0 +1,13 @@
+let
+  # master on 2021-08-01
+  rev = "9fc2cddf24ad1819f17174cbae47789294ea6dc4";
+
+  sha256 = "058l6ry119mkg7pwmm7z4rl1721w0zigklskq48xb5lmgig4l332";
+
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
+
+in
+import nixpkgs { config = { }; }
--- shell.nix
+++ shell.nix
@@ -1,8 +1,4 @@
-let pkgs = import (builtins.fetchTarball {
-      # master on 2021-08-01
-      url = "https://github.com/NixOS/nixpkgs/archive/9fc2cddf24ad1819f17174cbae47789294ea6dc4.tar.gz";
-      sha256 = "058l6ry119mkg7pwmm7z4rl1721w0zigklskq48xb5lmgig4l332";
-    }) { };
+let pkgs = import ./nix/pkgs.nix;
     ghcidScript = pkgs.writeShellScriptBin "dev" "ghcid --command 'cabal new-repl lib:matchmaker' --allow-eval --warnings -o ghcid.text";
     formatScript = pkgs.writeShellScriptBin "format" "stylish-haskell -ir ./**/*.hs";
     runScript = pkgs.writeShellScriptBin "run" "cabal run exe:matchmaker";
